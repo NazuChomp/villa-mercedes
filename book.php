fbc108583e3b6dc5d987c9c609284f10dbb9e360
codesnishaun <shaunaaronnaga.basc@gmail.com>
2026-04-26 10:29:08 +0800

    feat: facility info and form

--- book.php
+++ book.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 require_once 'utils/helper.php';
+require_once 'database/database.php';
 
 $currentPage = basename($_SERVER['PHP_SELF']);
+
+$conn = connection();
+$stmt = $conn->prepare("SELECT * FROM facilities");
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +31,41 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 </a>
             </div>
         </div>
-    </header>
+    </header>   
+    
+    <main class="main-booking">
+        <div class="header-block">
+            <h2>Reserve your stay</h2>
+            <p>select your preferred accommodation and fill out the details below</p>
+        </div>
+        <div class="facility-card-selector">
+            <div class="facility-card">
+                <?php if($result->num_rows > 0): ?>
+                    <?php while($row = $result->fetch_assoc()): ?>
+                        <div class="facility-card">
+                            <img src="<?= e(url('resources/img/') . $row['image_filename']) ?>" 
+                                 alt="<?= e($row['image_filename']) ?>"
+                                 width="100px">
+                            <div class="card-info">
+                                <h3><?= e($row['name']) ?></h3>
+                                <p><?= e($row['description']) ?></p>
+                                <p><?= e('₱' . $row['rate_amount']) ?></p>
+                                <p>click for more details</p>
+                            </div>
+                        </div>
+                    <?php endwhile;?>
+                <?php else: ?>
+                    <p class="no-data">No Facilities Yet</p>
+                <?php endif; ?>
+            </div>
+            <div class="booking-form">
+                <div class="form-row">
+                    <label for="fullname">Full Name</label>
+                    <input type="text" name="fullname" id="fullname">
+                </div>  
+            </div>
+        </div>
+    </main>
+
 </body>
 </html>
