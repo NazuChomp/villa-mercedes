787f4895decb0687fe1992f7d8d888e432d059f7
codesnishaun <shaunaaronnaga.basc@gmail.com>
2026-05-11 12:40:12 +0800

    refactor: change the data type of text paramater on flash function to array

--- utils/helper.php
+++ utils/helper.php
@@ -19,7 +19,7 @@ function e(?string $value): string
     return htmlspecialchars((string) $value, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 }
 
-function flash(string $type, string $text): void 
+function flash(string $type, array $text): void 
 {
     $_SESSION['flash'] = [
         'type' => $type,
